fa9141c3d2cd2ff68b104210bbe94161387d24b4
Zach Dwiel <zdwiel@plotwatt.com>
2015-11-08 02:28:54 +0000

    depricate requirements.txt

--- setup.py
+++ setup.py
@@ -1,11 +1,5 @@
 from setuptools import setup
-from pip.req import parse_requirements
 
-# reqs is a list of requirement
-# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
-install_requires = [
-    str(ir.req) for ir in parse_requirements('requirements.txt', session=False)
-]
 
 setup(
     name='sql_query_dict',
@@ -16,5 +10,5 @@ setup(
     author_email='zdwiel@plotwatt.com',
     license='Apache',
     py_modules=['sql_query_dict'],
-    install_requires=install_requires,
+    install_requires=['six'],
 )

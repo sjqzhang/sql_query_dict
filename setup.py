21ae0596f91f2b81380db5225035d33acc5f536e
Zach Dwiel <zdwiel@plotwatt.com>
2015-12-13 17:38:39 -0500

    increment version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='sql_query_dict',
-    version='0.6',
+    version='0.7',
     description='express sql queries as python dictionaries',
     url='http://github.com/PlotWatt/sql_query_dict',
     author='PlotWatt',
